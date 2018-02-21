ba629f33e5c36abf335de88eed31096dc34811d2
Achille Urbain <achille.urbain@gmail.com>
2018-02-21 02:13:14 +0100

    Update into

--- into.js
+++ into.js
@@ -1,8 +1,8 @@
 const curry = require('./curry')
-const reduce = require('./reduceSync')
+const transduce = require('./transduce')
 const getAddReducer = require('./internal/getAddReducer')
 
 const into = (accumulator, transformer, collection) =>
-  reduce(transformer(getAddReducer(collection)), accumulator, collection)
+  transduce(transformer, getAddReducer(collection), accumulator, collection)
 
 module.exports = curry(into)
