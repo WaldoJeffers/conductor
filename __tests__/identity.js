f7d37255033634054e6a6f04ba70b963fc172320
Achille Urbain <achille.urbain@gmail.com>
2018-03-08 21:07:10 +0100

    Add tests for identity

---
+++ __tests__/identity.js
@@ -0,0 +1,8 @@
+const identity = require('../identity')
+
+describe('identity', () => {
+  it('should return the first passed parameter', () => {
+    const values = [2, 'drumsticks', new Date(), () => 'world', {}]
+    expect(values.map(identity)).toEqual(values)
+  })
+})
