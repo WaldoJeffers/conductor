9bfeb79f5e0ba0d55833eace9abef354c4c323dc
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2018-03-27 00:27:15 +0200

    Fix tests for upsert

--- __tests__/upsert.js
+++ __tests__/upsert.js
@@ -8,12 +8,19 @@ describe('upsert', () => {
     expect(upsert(({ id }) => id === 2, han, [luke])).toEqual([luke, han])
   })
 
-  it('should replace an item to a collection which does not contain it', () => {
+  it('should replace an item in a collection which contains it', () => {
     expect(
       upsert(({ id }) => id === 2, han, [
         luke,
         { id: 2, firstname: 'Chewbacca' },
       ])
     ).toEqual([luke, han])
+  })
+
+  it('should be a pure function', () => {
+    const input = [luke]
+    const output = upsert(({ id }) => id === 2, han, input)
+    expect(input).toEqual([luke])
+    expect(output).not.toBe(input)
   })
 })
