a1f7176fb5ae74da0c0d829eaed0906c6fb38e69
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2018-03-27 00:56:29 +0200

    Fix reduce

--- src/reduce.js
+++ src/reduce.js
@@ -6,7 +6,7 @@ const reduce = reducer => (acc, value, key, collection) =>
 
 const reduceAll = (reducer, seed, collection) =>
   Array.isArray(collection)
-    ? collection.reduce(reduce(reducer))
+    ? collection.reduce(reduce(reducer), seed)
     : entries(collection).reduce(
       (acc, [key, value]) => reduce(reducer)(acc, value, key, collection),
       seed
