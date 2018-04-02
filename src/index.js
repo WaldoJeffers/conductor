2ab7372a1eb7661eb3f677258939952fb7abcea1
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2018-04-02 23:34:03 +0200

    Export tested & documented methods

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const always = require('./always')
+const append = require('./append')
+const apply = require('./apply')
+const arity = require('./arity')
+const branch = require('./branch')
+const capitalize = require('./capitalize')
+const compose = require('./compose')
+const curry = require('./curry')
+const curryN = require('./curryN')
+const identity = require('./identity')
+const delay = require('./delay')
+
+module.exports = {
+  always,
+  append,
+  apply,
+  arity,
+  branch,
+  capitalize,
+  compose,
+  curry,
+  curryN,
+  delay,
+  identity,
+}
