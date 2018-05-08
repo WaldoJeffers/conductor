bc7facd68c3ffabaaecdfd94792ad85dd661fac1
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2018-05-09 00:54:05 +0200

    Fix delay for Node v10

--- __tests__/delay.js
+++ __tests__/delay.js
@@ -1,16 +1,14 @@
-const { performance } = require('perf_hooks')
 const delay = require('../src/delay')
 
 describe('delay', () => {
   it('delays the value by the specified duration', async () => {
-    performance.mark('delay start')
+    const start = Date.now()
     const result = await delay(500, 'hello')
-    performance.mark('delay end')
-    performance.measure('delay start to end', 'delay start', 'delay end')
-    const measure = performance.getEntriesByName('delay start to end')[0]
+    const end = Date.now()
+    const duration = end - start
 
     expect(result).toBe('hello')
-    expect(measure.duration > 450 && measure.duration < 550).toBe(true)
+    expect(duration > 450 && duration < 550).toBe(true)
   })
 
   it('returns a promise', () => {
