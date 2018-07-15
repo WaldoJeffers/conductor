1c2c4ef9d86ade4b3d4ebad5792e5f93d00a3a33
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2018-07-15 14:17:19 +0200

    Fix: make append work for sets

--- __tests__/append.js
+++ __tests__/append.js
@@ -5,6 +5,12 @@ describe('append', () => {
     expect(append('world', ['hello'])).toEqual(['hello', 'world'])
   })
 
+  it('should add an element to a set', () => {
+    expect(append('world', new Set(['hello']))).toEqual(
+      new Set(['hello', 'world'])
+    )
+  })
+
   it('should be a pure function', () => {
     const input = ['hello']
     const output = append('world', input)
--- src/append.js
+++ src/append.js
@@ -1,5 +1,14 @@
 const curry = require('./curry')
+const type = require('./type')
 
-const append = (item, array) => [...array, item]
+const append = (item, collection) => {
+  switch (type(collection)) {
+  case 'set':
+    return new Set([...collection, item])
+  case 'array':
+  default:
+    return [...collection, item]
+  }
+}
 
 module.exports = curry(append)
