b0d9746228f1524eec4e09790d0da9765e62d712
AlexToudic <alexandre.toudic@gmail.com>
2018-12-27 23:49:29 +0100

    Use  and  in equals

--- src/equals.js
+++ src/equals.js
@@ -1,5 +1,7 @@
 const curry = require('./curry')
 const type = require('./type')
+const every = require('./every')
+const some = require('./some')
 
 const equals = (a, b) => {
   if (a === b) {
@@ -9,32 +11,31 @@ const equals = (a, b) => {
     const type_b = type(a)
     if (type_a === type_b) {
       switch (type(a)) {
-      case 'array':
-        return (
-          a.length === b.length &&
-            a.every((value, index) => equals(value, b[index]))
-        )
-      case 'set':
-        return (
-          a.size === b.size &&
-            [...a].every(value_a =>
-              [...b].some(value_b => equals(value_a, value_b))
-            )
-        )
-      case 'map':
-        return (
-          a.size === b.size &&
-            [...a.keys()].every(key => equals(a.get(key), b.get(key)))
-        )
-      case 'object':
-        return (
-          Object.keys(a).length === Object.keys(b).length &&
-            Object.keys(a).every(key => equals(a[key], b[key]))
-        )
-      case 'date':
-        return a.getTime() === b.getTime()
-      default:
-        break
+        case 'array':
+          return (
+            a.length === b.length &&
+            every((value, index) => equals(value, b[index]), a)
+          )
+        case 'set':
+          return (
+            a.size === b.size &&
+            every(value_a =>
+              some(value_b => equals(value_a, value_b), b)
+              , a)
+          )
+        case 'map':
+          return (
+            a.size === b.size && every(key => equals(a.get(key), b.get(key)), a.keys())
+          )
+        case 'object':
+          return (
+            Object.keys(a).length === Object.keys(b).length &&
+            every(key => equals(a[key], b[key]), Object.keys(a))
+          )
+        case 'date':
+          return a.getTime() === b.getTime()
+        default:
+          break
       }
     }
   }
