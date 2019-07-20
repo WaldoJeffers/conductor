906230e8b6aaf87e21affbc86fedb19546d7b35c
Achille Urbain <7644970+WaldoJeffers@users.noreply.github.com>
2019-07-20 19:01:05 +0200

    fix: Provide a numerical index as the fifth argument of reduce

--- src/reduce.js
+++ src/reduce.js
@@ -2,14 +2,14 @@ const curry = require('./curry')
 const entries = require('./entries')
 const then = require('./then')
 
-const reduce = reducer => (acc, value, key, collection) =>
-  then(result => reducer(result, value, key, collection), acc)
+const reduce = reducer => (acc, value, key, collection, index) =>
+  then(result => reducer(result, value, key, collection, index), acc)
 
 const reduceAll = (reducer, seed, collection) =>
   Array.isArray(collection)
     ? collection.reduce(reduce(reducer), seed)
     : entries(collection).reduce(
-      (acc, [key, value]) => reduce(reducer)(acc, value, key, collection),
+      (acc, [key, value], index) => reduce(reducer)(acc, value, key, collection, index),
       seed
     )
 
